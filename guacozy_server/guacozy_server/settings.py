7fed2ae3264e51670076242d0137a8ea6bd384fe
Pavel Paidem <pavel.paidem@gmail.com>
2019-10-09 01:49:45 +0300

    server: init custom user model

--- guacozy_server/guacozy_server/settings.py
+++ guacozy_server/guacozy_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
