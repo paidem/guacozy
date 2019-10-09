0b251d4903ff961564a214b01408f3c3ab6a04fe
Pavel Paidem <pavel.paidem@insoft.lt>
2019-10-09 19:26:42 +0300

    server: add Ticket per object permission

--- guacozy_server/backend/rules.py
+++ guacozy_server/backend/rules.py
@@ -12,3 +12,23 @@ rules.add_perm('backend.add_personalnamedcredentials', is_owner)
 rules.add_perm('backend.view_personalnamedcredentials', is_owner)
 rules.add_perm('backend.change_personalnamedcredentials', is_owner)
 rules.add_perm('backend.delete_personalnamedcredentials', is_owner)
+
+
+@rules.predicate
+def can_see_ticket(user, obj):
+    if obj is None:
+        return True
+    return obj.author == user or obj.user == user
+
+
+@rules.predicate
+def is_ticket_user_or_author(user, obj):
+    if obj is None:
+        return False
+    return obj.author == user or obj.user == user
+
+
+rules.add_perm('backend.add_ticket', is_ticket_user_or_author)
+rules.add_perm('backend.view_ticket', is_ticket_user_or_author)
+rules.add_perm('backend.change_ticket', is_ticket_user_or_author)
+rules.add_perm('backend.delete_ticket', is_ticket_user_or_author)
