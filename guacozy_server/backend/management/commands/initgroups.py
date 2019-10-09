3b5fe76f0ec4080618e83974a0084dd2e9a785fd
Pavel Paidem <pavel.paidem@gmail.com>
2019-10-09 23:48:23 +0300

    server: add initguacd / initgroups management commands

---
+++ guacozy_server/backend/management/commands/initgroups.py
@@ -0,0 +1,51 @@
+from django.core.management import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+from backend import models
+
+GROUPS_PERMISSIONS = {
+    'ConnectionsAdmin': {
+        models.StaticCredentials: ['add', 'change', 'delete', 'view'],
+        models.NamedCredentials: ['add', 'change', 'delete', 'view'],
+        models.PersonalNamedCredentials: ['add', 'change', 'delete', 'view'],
+        models.Folder: ['add', 'change', 'delete', 'view'],
+        models.FolderPermission: ['add', 'change', 'delete', 'view'],
+        models.ConnectionRdp: ['add', 'change', 'delete', 'view'],
+        models.ConnectionSsh: ['add', 'change', 'delete', 'view'],
+        models.GuacdServer: ['add', 'change', 'delete', 'view'],
+        models.AppSettings: ['view'],
+    },
+}
+
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+
+    help = "Create default groups"
+
+    def handle(self, *args, **options):
+        # Loop groups
+        for group_name in GROUPS_PERMISSIONS:
+
+            # Get or create group
+            group, created = Group.objects.get_or_create(name=group_name)
+
+            # Loop models in group
+            for model_cls in GROUPS_PERMISSIONS[group_name]:
+
+                # Loop permissions in group/model
+                for perm_index, perm_name in enumerate(GROUPS_PERMISSIONS[group_name][model_cls]):
+
+                    # Generate permission name as Django would generate it
+                    codename = perm_name + "_" + model_cls._meta.model_name
+
+                    try:
+                        # Find permission object and add to group
+                        perm = Permission.objects.get(codename=codename)
+                        group.permissions.add(perm)
+                        self.stdout.write("Adding {} to group {}"
+                                          .format(codename, group.__str__()))
+
+                    except Permission.DoesNotExist:
+                        self.stdout.write("{} not found".format(codename))
---
+++ guacozy_server/backend/management/commands/initguacd.py
@@ -0,0 +1,53 @@
+from django.core.management import BaseCommand
+
+from backend import models
+
+default_guacd_server_name = "guacd docker"
+default_guacd_server_hostname = "guacd"
+default_guacd_server_port = 4822
+
+
+class Command(BaseCommand):
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+
+    help = "Create default guacd server"
+
+    def handle(self, *args, **options):
+        # Get or create default GuacdServer
+        guacd_server, created = models.GuacdServer.objects \
+            .get_or_create(name=default_guacd_server_name)
+
+        self.stdout.write("\nChecking if we need to create '{}'."
+                          .format(default_guacd_server_name))
+
+        if created:
+            self.stdout.write("'{}' not found, creating new with parameter:"
+                              .format(default_guacd_server_name))
+            self.stdout.write("name     = {}"
+                              .format(default_guacd_server_name))
+            self.stdout.write("hostname = {}"
+                              .format(default_guacd_server_hostname))
+            self.stdout.write("port     = {}"
+                              .format(default_guacd_server_port.__str__()))
+            guacd_server.hostname = default_guacd_server_hostname
+            guacd_server.port = default_guacd_server_port
+            guacd_server.save()
+        else:
+            self.stdout.write("'{}' exists. Skipping."
+                              .format(default_guacd_server_name))
+
+        # Load AppSettings (will be created if doesn't exist)
+        app_settings = models.AppSettings.load()
+
+        self.stdout.write("\nChecking if default Guacd server is set.")
+
+        if app_settings.defaultGuacdServer is None:
+            self.stdout.write("Default guacd not set. Setting to '{}'"
+                              .format(guacd_server.name))
+            app_settings.defaultGuacdServer = guacd_server
+            app_settings.save()
+        else:
+            self.stdout.write("Default guacd is already set ('{}'). Skipping."
+                              .format(app_settings.defaultGuacdServer.name))
