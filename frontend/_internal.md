024ea7d1cc5f3d47f576b61016e874445c8c76dc
Pavel Paidem <pavel.paidem@gmail.com>
2019-10-12 20:40:32 +0300

    frontend: create-react-app, cleanup, logo/favicon

---
+++ frontend/_internal.md
@@ -0,0 +1,1 @@
+[Favicon source](https://favicon.io/favicon-generator/?t=GC&ff=Homenaje&fs=130&fc=%23FF0&b=rounded&bc=%23000)
---
+++ frontend/src/Components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
