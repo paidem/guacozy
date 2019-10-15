8b1e8da69f11d8b86ee59b570ba1ac8913d34cc5
Pavel Paidem <pavel.paidem@gmail.com>
2019-10-15 14:37:14 +0300

    frontend: add react-contexify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'flexlayout-react/style/dark.css'
+import 'react-contexify/dist/ReactContexify.min.css';
 import App from './Components/App/App';
 import {AppProvider} from "./Context/AppContext";
 import {LayoutProvider} from "./Layout/LayoutContext";
