25173d9d6bbd5155a8a89bdb46630213e41dbf3e
Pavel Paidem <pavel.paidem@insoft.lt>
2019-10-22 17:47:05 +0300

    server: remove delete options from AppSettingsAdmin

--- guacozy_server/backend/admin/appsettings.py
+++ guacozy_server/backend/admin/appsettings.py
@@ -10,6 +10,9 @@ class AppSettingsModelAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def has_add_permission(self, request):
+        return False
+
     def get_actions(self, request):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
