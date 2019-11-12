865a4379f506dedabe1209209bcd4ca6f5b702c2
Pavel Paidem <pavel.paidem@gmail.com>
2019-11-12 14:38:58 +0200

    backend: admin, add inherited permissions view in folder. Resolve #22.

--- guacozy_server/backend/admin/folder.py
+++ guacozy_server/backend/admin/folder.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
 from django_mptt_admin.admin import DjangoMpttAdmin
 
 from backend.models import FolderPermission, Folder
@@ -8,10 +9,58 @@ class FolderPermissionInline(admin.TabularInline):
     model = FolderPermission
 
 
+class FolderInheritedPermissionInlineFormSet(BaseInlineFormSet):
+
+    def __init__(self, *args, **kwargs):
+        super(FolderInheritedPermissionInlineFormSet, self).__init__(*args, **kwargs)
+        # Get FolderPermission objects which reference any ancestor folders
+        # .order_by('folder__lft') make use of MPTT lft to make them appear in same order as in tree
+        self.queryset = FolderPermission.objects.filter(folder__in=kwargs['instance']
+                                                        .get_ancestors()).order_by('folder__lft')
+
+
+class FolderInheritedPermissionInline(admin.TabularInline):
+    model = FolderPermission
+    extra = 0
+
+    verbose_name_plural = 'Inherited permissions'
+
+    fields = ['get_folderbc', 'group', 'user', ]
+    readonly_fields = ['get_folderbc', ]
+
+    def get_folderbc(self, obj):
+        if obj.pk:
+            return FolderPermission.objects.get(pk=obj.pk).folder.breadcrumbs
+        else:
+            return ""
+
+    get_folderbc.short_description = "Defined in"
+    get_folderbc.allow_tags = True
+
+    formset = FolderInheritedPermissionInlineFormSet
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(super().get_fields(request, obj))
+
+
 @admin.register(Folder)
 class FolderModelAdmin(DjangoMpttAdmin):
     list_display = ['name', 'breadcrumbs']
     readonly_fields = ('breadcrumbs',)
     inlines = (
         FolderPermissionInline,
+        FolderInheritedPermissionInline,
     )
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['after_related_objects'] = "Hello there"
+        return super(FolderModelAdmin, self).render_change_form(request, context, args, kwargs)
